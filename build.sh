54bb70391a9b626e05c25618bca9ef516b05e3d6
naophis <nao1288stusj@gail.com>
2021-05-20 02:30:20 +0900

    ビルド時に指定するファイルの修正

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CURRENT=$(cd $(dirname $0);pwd)
+cd $CURRENT
+
 if [ $# != 1 ]; then
     echo "引数で数字を指定してください"
     exit 1;
@@ -7,11 +10,14 @@ fi
 
 if [ ! -e ${ACLib_dir} ]; then
     echo "atcoder-lib(ACL)がインストールされていません"
+    exit 1
 fi
 
-CURRENT=$(cd $(dirname $0);pwd)
-cd $CURRENT
+if [ ! -e ./main_${1}.cpp ]; then
+    echo "対象ファイルがありません main_${1}.cpp "
+    exit 1
+fi
 
-g++ no${1}.cpp -std=c++14 -I ${ACLib_dir} -o no${1} 
+g++ main_${1}.cpp -std=c++14 -I ${ACLib_dir} -o exe_${1} 
 
 exit 0
