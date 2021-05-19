590f460191680ff6d002c763a7acbcb9f13b2821
naophis <nao1288stusj@gail.com>
2021-05-20 02:25:09 +0900

    ファイル名変更

--- make_ws.sh
+++ make_ws.sh
@@ -10,12 +10,12 @@ if [ ${file_cnt} -ge 3 ]; then
     exit 1
 else
     echo "make workspace ..."
-    cp template/temprate.cpp no1.cpp
-    cp template/temprate.cpp no2.cpp
-    cp template/temprate.cpp no3.cpp
-    cp template/temprate.cpp no4.cpp
-    cp template/temprate.cpp no5.cpp
-    cp template/temprate.cpp no6.cpp
+    cp template/temprate.cpp main_a.cpp
+    cp template/temprate.cpp main_b.cpp
+    cp template/temprate.cpp main_c.cpp
+    cp template/temprate.cpp main_d.cpp
+    cp template/temprate.cpp main_e.cpp
+    cp template/temprate.cpp main_f.cpp
     echo "...end"
     exit 0
 fi
